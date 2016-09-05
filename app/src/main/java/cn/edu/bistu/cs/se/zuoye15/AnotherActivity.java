f2fb075a8d3de89a9d3da036bac3ee416a99aba1
AKiraDDD <923771143@qq.com>
2016-09-05 13:13:00 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/zuoye15/AnotherActivity.java
@@ -0,0 +1,14 @@
+package cn.edu.bistu.cs.se.zuoye15;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2016/9/5.
+ */
+public class AnotherActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
